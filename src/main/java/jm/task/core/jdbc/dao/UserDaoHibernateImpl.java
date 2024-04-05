7d1c53b340d95ded962d97737a6c2d61ae1842ea
Maksim <yaroshevichmaks@gmail.com>
2024-04-05 10:11:02 +0300

    add rollback

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -24,48 +24,52 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Transaction transaction = session.beginTransaction();
         try {
-            Transaction transaction = session.beginTransaction();
             String query = "create table if not exists user(id integer not null primary key auto_increment, name varchar(30) not null, lastName varchar(30), age integer not null)";
             session.createSQLQuery(query).addEntity(User.class).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
+            transaction.rollback();
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = session.beginTransaction();
         try {
-            Transaction transaction = session.beginTransaction();
             String query = "drop table if exists user";
             session.createSQLQuery(query).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
+            transaction.rollback();
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = session.beginTransaction();
         try {
-            Transaction transaction = session.beginTransaction();
             User user = new User(name, lastName, age);
             session.save(user);
             transaction.commit();
         } catch (Exception e) {
+            transaction.rollback();
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = session.beginTransaction();
         try {
-            Transaction transaction = session.beginTransaction();
             User user = session.get(User.class, id);
             session.delete(user);
             transaction.commit();
         } catch (Exception e) {
+            transaction.rollback();
             throw new RuntimeException(e);
         }
     }
@@ -73,8 +77,8 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
+        Transaction transaction = session.beginTransaction();
         try {
-            Transaction transaction = session.beginTransaction();
             CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
             CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
             criteriaQuery.select(criteriaQuery.from(User.class));
@@ -82,6 +86,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
             transaction.commit();
         } catch (Exception e) {
+            transaction.rollback();
             throw new RuntimeException(e);
         }
         return users;
@@ -89,14 +94,15 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
+        Transaction transaction = session.beginTransaction();
         try {
             CriteriaDelete criteriaDelete = session.getCriteriaBuilder().createCriteriaDelete(User.class);
             criteriaDelete.from(User.class);
 
-            Transaction transaction = session.beginTransaction();
             session.createQuery(criteriaDelete).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
+            transaction.rollback();
             throw new RuntimeException(e);
         }
     }
